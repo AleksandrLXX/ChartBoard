5081ff083f933b665dea41467bc7fcd4d3362d7e
DESKTOP-G8EJQ7M\zjlin <zjlinxiangxiang@qq.com>
2018-09-26 08:14:26 +0800

    环形图

--- src/pages/common/js/ring_render.js
+++ src/pages/common/js/ring_render.js
@@ -1,54 +1,65 @@
 import echarts from 'echarts'
 
 var mergeOption = function (data, title) {
-    let _data = data
-    let max = _data.map(item => Number(item.value)).reduce((prev, next) => {
-        next = prev + next
-        return next
-    }, 0)
+    let _data = data.sort((prev,next)=>next.name==title?1:-1)
     return {
-
-        title: {
-            text: title,
-            show: false,
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b}: {c} ({d}%)"
         },
-        tooltip: {},
+        title:{
+            text:title,
+            top:'bottom',
+            show:true,
+            left:"center",
+        },
         legend: {
-            show: false,
-        },
-        radar: {
-            // shape: 'circle',
-            name: {
-                textStyle: {
-                    color: '#fff',
-                    // backgroundColor: '#999',
-                    borderRadius: 3,
-                    padding: [3, 5]
-                }
-            },
-            indicator: _data.map(item => {
-                return {
-                    name: item.key,
-                    max: max
-                }
-            })
+            show:false,
+            orient: 'vertical',
+            x: 'left',
+            data:_data.map(item => item.name)
         },
         series: [{
-            name: '成果数量',
-            type: 'radar',
-            // areaStyle: {normal: {}},
-            data: [{
-                value: _data.map(item => item.value),
-                name: '成果数量'
-            }]
+            name: '成果数量(分学科)',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: true,
+                    position: 'center',
+                    textStyle: {
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    },
+                    formatter: function (params) {
+                        return params.dataIndex==0 ? params.value :''
+                    }
+                },
+                emphasis: {
+                    show: false,
+                    textStyle: {
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    },
+                    formatter:'{c}'
+                    
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data: _data
         }]
     }
 }
-var radar_render = (id, data, title) => {
+var ring_render = (id, data, title) => {
     var chart = echarts.init(document.getElementById(id), 'user');
     // 绘制图表
     chart.setOption(mergeOption(data, title));
     return chart;
 }
 
-export default radar_render;
+export default ring_render;
--- src/pages/index/js/index.js
+++ src/pages/index/js/index.js
@@ -1,6 +1,6 @@
 import  '../style/bootstrap-reboot.css'
 import  '../style/bootstrap-grid.css'
-
+import jQuery from 'jquery'
 import '../style/theme.css'
 
 import echarts from 'echarts'
@@ -11,8 +11,10 @@ import line_render from '../../common/js/line_render.js'
 import interval_render from '../../common/js/interval_render.js'
 import radar_render from '../../common/js/radar_render.js'
 import mixed_render from '../../common/js/mixed_render.js'
+import ring_render from '../../common/js/ring_render.js'
 import board1Data from './fake.js'
 
+window.$= jQuery
 // 需要是 echarts 4 的配置方法
 echarts.registerTheme('user', userTheme)
 
@@ -67,12 +69,32 @@ var chart4Data = board1Data['高被引及热点论文数量'].reduce((prev,next)
     target[next["类型"]] = Number(next["成果数量"])
     return prev;
 },[])
+
+var chart5Data = (function(arr) {
+    let obj = {};
+    arr.map(item => {
+        let key = item["论文类型"] ? "论文类型" : "成果类型";
+        obj[item[key]] = obj[item[key]] || 0;
+        obj[item[key]] += Number(item["成果数量"])
+    })
+    if ("期刊论文" in obj || "会议论文" in obj || "学术论文" in obj){
+        delete obj["论文"]
+    }
+    return Object.keys(obj).map(item=>{ return {name:item,value:obj[item]}})
+})(board1Data['成果数量(分学科)']);
+
+console.log(chart5Data)
 console.log(chart3Data)
 console.log(chart4Data)
 var chart1 = line_render('成果数量(分年)', chart1Data['期刊论文'], '期刊论文')
 var chart2 = interval_render('成果数量(分学科)',chart2Data,Array.from(chart2Series))
 var chart3 = radar_render('成果数量(分数据库)', chart3Data, '成果数量(分数据库)')
 var chart4 = mixed_render('高被引及热点论文数量', chart4Data)
+let c5l = chart5Data.length
+var chart5 = chart5Data.map((item,index)=>{
+    $('.board1-col-2-row-1').append(` <div id='成果数量(分学科)${index}' class='canvas-container flex-1' style='width:${100/c5l}%'></div>`)
+    ring_render(`成果数量(分学科)${index}`, chart5Data, item.name)
+}) 
 
 var chart6 = map_ch_render('国内合作',board1Data['国内合作'])
 console.log(echarts)
